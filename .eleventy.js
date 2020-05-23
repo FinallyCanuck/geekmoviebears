42becb9ad83276f0b7af31779ff5f809674423bc
Peter Randazzo <pete@randazzo.ca>
2020-05-23 17:27:44 +0100

    Adding film posters

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,21 @@
-var moment = require("moment-timezone");
+var moment = require('moment-timezone');
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy('src/js');
+  eleventyConfig.addPassthroughCopy('src/css');
 
-  eleventyConfig.addFilter("dateformat", function (dateIn) {
-    return moment(dateIn).tz("Europe/London").format("DD-MMMM-Y HH:mm");
+  eleventyConfig.addFilter('dateformat', function (dateIn) {
+    return moment(dateIn).tz('Europe/London').format('Y-MM-DD HH:mm');
   });
 
   return {
     dir: {
-      output: "dist",
-      input: "src",
-      data: "jsondata",
-      includes: "partials_layouts",
+      output: 'dist',
+      input: 'src',
+      data: 'jsondata',
+      includes: 'partials_layouts',
     },
 
-    templateFormats: ["njk", "md"],
+    templateFormats: ['njk', 'md'],
   };
 };
