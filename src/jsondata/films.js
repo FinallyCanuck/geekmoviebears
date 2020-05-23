ec75ec45217ec81904696ada520c2fd3c13078e9
Peter Randazzo <pete@randazzo.ca>
2020-05-23 16:40:56 +0100

    Removing console log for film

--- src/jsondata/films.js
+++ src/jsondata/films.js
@@ -7,6 +7,5 @@ module.exports = async function () {
     `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_API_KEY}&language=en-GB&region=GB`
   );
   let films = await res.json();
-  console.log(films.results);
   return films.results;
 };
