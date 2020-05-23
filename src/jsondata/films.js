37172dc6be22927bb1fa9e7f847d39dc5d073707
Peter Randazzo <pete@randazzo.ca>
2020-05-23 16:35:57 +0100

    Tweaks to API results

--- src/jsondata/films.js
+++ src/jsondata/films.js
@@ -4,8 +4,9 @@ const fetch = require('node-fetch');
 
 module.exports = async function () {
   let res = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&language=en-GB&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_API_KEY}&language=en-GB&region=GB`
   );
   let films = await res.json();
+  console.log(films.results);
   return films.results;
 };
