1f4d38d4b0232ef4d3b4697c7e3a1c4c4365747b
Peter Randazzo <pete@randazzo.ca>
2020-05-23 15:24:53 +0100

    Trying to get the API to display data

--- src/jsondata/tmdb.js
+++ src/jsondata/tmdb.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 let axios = require('axios');
 
 module.exports = async function () {
-  let url = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.TMDB_API_KEY}`;
+  let url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&language=en-GB&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
 
   return axios
     .get(url)
