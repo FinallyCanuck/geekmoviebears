238819600b647df493cc8754ab71a4f5d37e82ee
Peter Randazzo <pete@randazzo.ca>
2020-05-25 16:42:23 +0100

    Adding poster not found handling

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ var moment = require('moment-timezone');
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/js');
   eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/img');
 
   eleventyConfig.addFilter('dateformat', function (dateIn) {
     return moment(dateIn).tz('Europe/London').format('Y-MM-DD HH:mm');
--- src/jsondata/settings.js
+++ src/jsondata/settings.js
@@ -1,4 +1,5 @@
 module.exports = {
   siteName: 'GeekMovieBears',
   facebook: 'https://www.facebook.com/groups/geekmoviebears/',
+  twitter: 'https://twitter.com/GeekMovieBears',
 };
